0b7a07ffb4d54744afbb03233f0ceba267f26aad
Msu global <mshafeulla@gmail.com>
2026-02-04 18:00:54 +0530

    initial commit

---
+++ List operations.py
@@ -0,0 +1,17 @@
+# How can we append inside the list
+Name = ["Vinod", "Stephen", "George", "Mohammed"]
+Name.append("Shafeeulla")
+print(Name)
+
+#How can we extend the list
+Names = ["Shahid"]
+Name.extend(Names)
+print(Name)
+
+#Remove item from the list
+Name.remove("Vinod")
+print(Name)
+
+#Pop item from the list
+Name.pop()
+print(Name)
