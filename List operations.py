1d5032bf0142f96fe5e80e2a4a2a6113b1f88a71
Msu global <mshafeulla@gmail.com>
2026-02-19 15:06:58 +0530

    Revision

--- List operations.py
+++ List operations.py
@@ -1,4 +1,9 @@
 # How can we append inside the list
+from itertools import count
+
+from decorator import append
+from dill import extend
+
 Name = ["Vinod", "Stephen", "George", "Mohammed"]
 Name.append("Shafeeulla")
 print(Name)
@@ -15,3 +20,30 @@ print(Name)
 #Pop item from the list
 Name.pop()
 print(Name)
+
+#How to insert element at a specific index
+Name.insert(2,"Md")
+print(Name)
+
+#To count specific elements in a list
+No = Name.count("Md")
+print(No)
+
+#To remove element from a list
+Name.remove("Md")
+print(Name)
+
+#To pop element from the list
+Name.pop(2)
+print(Name)
+
+#How to sort elements in a list
+Name.sort()
+print(Name)
+
+#How to reverse elements in a list
+Name.reverse()
+print(Name)
+
+#Membership operators
+print('Stephen' in Name)
