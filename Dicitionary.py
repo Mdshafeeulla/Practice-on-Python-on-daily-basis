8db2ad207f38ba5bed205e62fe8d3bb95feb4839
Msu global <mshafeulla@gmail.com>
2026-02-25 15:56:20 +0530

    Revision

--- Dicitionary.py
+++ Dicitionary.py
@@ -1,1 +1,3 @@
-Dictionary uses key value pairs and is represented by "{ }"
+# Dictionary uses key value pairs and is represented by "{ }" and is mutable but the keys cannot have
+# duplicates but the values can take any data type and also can have duplicate values
+
