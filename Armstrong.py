0ad115df4feabd4285c06346fc460974004bfb35
Msu global <mshafeulla@gmail.com>
2026-02-25 15:54:25 +0530

    Revision

--- Armstrong.py
+++ Armstrong.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def armstrong(self,num):
-        power = len(num)
-        int_num = int(num)
-        nums = 0
-        for i in num:
-            nums += int(i) ** power
-        if nums == int_num:
-            print("The given number is an armstrong")
-        else:
-            print("The given number is not an armstrong")
-arm_Solution = Solution()
-arm_Solution.armstrong("153")
+def armstrong(number):
+    power = len(number)
+    sum = 0
+    fixed_num = int(number)
+    for i in number:
+        sum += int(i) ** power
+    if sum == fixed_num:
+        print("It is armstrong")
+    else:
+        print("Not an armstrong")
+armstrong("123")
