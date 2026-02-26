8fb8808ec85ca029bc2a120c2c26c4308f126ca9
Msu global <mshafeulla@gmail.com>
2026-02-26 16:01:34 +0530

    Finding out wheather the length is even or odd to find out the median

--- Median of two sorted arrays.py
+++ Median of two sorted arrays.py
@@ -3,6 +3,15 @@ class Solution:
         nums1.extend(nums2)
         nums1.sort()
         print(nums1)
+        if len(nums1) % 2 == 0:
+            median1 = len(nums1) / 2
+            print(median1)
+        else:
+            med = len(nums1) / 2
+            median = round(med)
+            print(nums1[median])
+
+
 
 sol = Solution()
-sol.findMedianSortedArrays([1,2,3],[4,3,2])
+sol.findMedianSortedArrays([1,10,22,55,13,11,15,19],[4,1,1])
