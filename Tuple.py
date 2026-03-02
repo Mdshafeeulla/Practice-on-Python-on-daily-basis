e742fbd6dc89420cb690e47480106441ef0f4b5b
Msu global <mshafeulla@gmail.com>
2026-03-02 23:10:25 +0530

    Explaination

--- Tuple.py
+++ Tuple.py
@@ -1,3 +1,4 @@
 # The main difference between tuple and list is list mutable whereas tuple is not and list is represented by
 # "[]" whereas tuple is represented by "()"
-
+tup = ("Name","Name","is","Md","Shafeeulla",("My","usn"))
+print(tup[5][1])
