6e0c70a6fc2ce27f7fdffc84c425e17fe68b5bf3
Msu global <mshafeulla@gmail.com>
2026-03-04 14:55:04 +0530

    Firstly iterating over the given string and declaring a dictionary . if the loop index matched with the key in the dictionary then checking for the right very next index if it same as that of dictionary value then we increase it by +2

--- longest valid parentesis.py
+++ longest valid parentesis.py
@@ -1,13 +1,20 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        match = {
+            "{": "}",
+            "(": ")",
+            "[": "]"
+        }
+        count = 0
         for i in range(0,len(s) - 1):
-            count = 0
-            if s[i] == s[i + 1] :
-                count += 2
-                print(count)
-            else:
-                count = 0
-            print(count)
+            for exact_match in match:
+                if s[i] == exact_match and s[i + 1] == match[exact_match]:
+                    count += 2
+                else:
+                    count += 0
+        print(count)
+
+
 
 sol = Solution()
-sol.longestValidParentheses("{{}}")
+sol.longestValidParentheses("[{}[]}")
